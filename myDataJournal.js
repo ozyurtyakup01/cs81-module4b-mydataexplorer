35d03c13ab5345f344894f58de9f285e096b63a3
ozyurtyakup01 <ozyurt_yakup01@student.smc.edu>
2025-07-04 16:27:16 -0700

    highest screen and average sleep function updated

--- myDataJournal.js
+++ myDataJournal.js
@@ -24,14 +24,43 @@ Yes
 
 */
 
-function findHighestScreenTime()
-{
+const weekData =
+[
+{day: "Monday", sleepHours:8, screenTime:3, mood:"Bad", caffeineIntake:3, focusLevel:6},
+{day: "Tuesday", sleepHours:7, screenTime:4, mood:"Bad", caffeineIntake:3, focusLevel:4},
+{day: "Wednesday", sleepHours:8, screenTime:4, mood:"Bad", caffeineIntake:5, focusLevel:8},
+{day: "Thursday", sleepHours:7, screenTime:2, mood:"Bad", caffeineIntake:4, focusLevel:3},
+{day: "Friday", sleepHours:6, screenTime:4, mood:"Bad", caffeineIntake:3, focusLevel:5},
+{day: "Saturday", sleepHours:6, screenTime:4, mood:"Good", caffeineIntake:1, focusLevel:5},
+{day: "Sunday", sleepHours:8, screenTime:6, mood:"Good", caffeineIntake:1, focusLevel:1},
+] 
 
+function findHighestScreenTime(log)
+{
+    let maxTime = null;
+    //iterate each element    
+    for (let index of log)
+    {
+        // if current index screentime higher than max
+        if(index.screenTime > maxTime)
+        {
+            // new max time is current index
+            maxTime = index.screenTime;
+        }
+    }
 }
 
 function averageSleep()
 {
-    
+    let totalSleep = null;
+    //iterate each element 
+    for (let index of log)
+    {
+        // add each elements sleepHours object to total
+        totalSleep += index.sleepHours
+    }
+    //calculate average
+    return totalSleep / log.length() + totalSleep % log.length();
 }
 
 function mostFrequentMood()
