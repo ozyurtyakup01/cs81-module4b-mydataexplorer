b15b9c9b18ff97d8abf1985ee272fc4b2421b248
ozyurtyakup01 <ozyurt_yakup01@student.smc.edu>
2025-07-04 15:57:03 -0700

    Initial commit

---
+++ REFLECTION.md
@@ -0,0 +1,4 @@
+Were your predictions right?
+What surprised you?
+What was tricky or rewarding?
+How did using your own data change things?
---
+++ myDataJournal.js
@@ -0,0 +1,45 @@
+/*
+
+In myDataJournal.js, define an array named weekData with 7 objects, one for each day. Use your real or realistic data:
+
+day (string)
+sleepHours (number)
+screenTime (hours)
+mood (string)
+caffeineIntake (cups)
+focusLevel (1–10)
+
+*/
+
+
+/*
+Add comments in your code predicting:
+
+Which day had the most screen time?
+Tuesday
+Best focus day?
+Sunday
+Is more caffeine helping?
+Yes
+
+*/
+
+function findHighestScreenTime()
+{
+
+}
+
+function averageSleep()
+{
+    
+}
+
+function mostFrequentMood()
+{
+    
+}
+
+function correlateCaffeineToFocus()
+{
+    
+}
