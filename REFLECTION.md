9ee5da7b13fc54fffa6a7679d737e6c9f74349a8
ozyurtyakup01 <ozyurt_yakup01@student.smc.edu>
2025-07-05 15:44:53 -0700

    final functions and reflection

--- REFLECTION.md
+++ REFLECTION.md
@@ -1,4 +1,26 @@
 Were your predictions right?
+Mostly no, results below
+
 What surprised you?
+Nothing, I've made up the data 
+
 What was tricky or rewarding?
+Coding practice was rewarding
+
 How did using your own data change things?
+It made the results predictable and fixed. 
+
+
+Prediction
+
+Highest Screen time: Tuesday
+Best focus day: Sunday
+Is more caffeine helping?
+Yes
+
+Results
+
+Average sleep time:  7.14  hours
+Highest Screen time:  Sunday, 6  hours
+Most frequent mood: Bad
+Wednesday was most caffeine intake and focus level
--- myDataJournal.js
+++ myDataJournal.js
@@ -53,8 +53,6 @@ function findHighestScreenTime(log)
     return whichDay + ", " + maxTime;
 }
 
-console.log("Highest Screen time: ",findHighestScreenTime(weekData)," hours");
-
 function averageSleep(log)
 {
     let totalSleep = 0;
@@ -68,14 +66,74 @@ function averageSleep(log)
     return ((totalSleep / log.length).toFixed(2));
 }
 
+function mostFrequentMood(log)
+{
+   // object to record frequency
+  const frequency = {};
+  // iterate each element of given log
+  for (let index of log) 
+    {
+        // check if the element already counted
+        if (!frequency[index.mood]) 
+        {
+        //if not, update count to 1
+        frequency[index.mood] = 1;
+        } 
+        else 
+        {
+        // if already counted, increase count by 1
+        frequency[index.mood]++;
+        }
+    }
+
+  let mostFrequent = null;
+  let maxCount = 0;
+  for (let mood in frequency) {
+    // if current element count greater then max,
+    if (frequency[mood] > maxCount) 
+    {
+      // current element new most frequent
+      mostFrequent = mood;
+      // new max count is current element's count
+      maxCount = frequency[mood];
+    }
+  }
+  return mostFrequent;
+}
+
+function correlateCaffeineToFocus(log)
+{
+    let maxCaf = 0;
+    let maxFocus = 0;  
+    let maxCafDay = "";
+    let maxFocusDay = "";
+
+    for (let index of log)
+    {   
+        // Check max caffeine intake
+        if(index.caffeineIntake > maxCaf)
+        {
+            maxCaf = index.caffeineIntake;
+            maxCafDay = index.day;
+        }
+
+        // Check max focus
+        if(index.focusLevel > maxFocus)
+        {
+            maxFocus = index.focusLevel;
+            maxFocusDay = index.day;
+        }
+    }
+    // Check if caffeine intake and focus level are related
+    if(maxCafDay === maxFocusDay)
+    {
+        return maxFocusDay +" was most caffeine intake and focus level";
+    }
+    
+    return "Max Focus Day: " + maxFocusDay +" Most Caffeine intake at: " + maxCafDay;
+}
+
 console.log("Average sleep time: ",averageSleep(weekData)," hours");
-
-function mostFrequentMood()
-{
-    
-}
-
-function correlateCaffeineToFocus()
-{
-    
-}
+console.log("Highest Screen time: ",findHighestScreenTime(weekData)," hours");
+console.log("Most frequent mood:", mostFrequentMood(weekData));
+console.log(correlateCaffeineToFocus(weekData));
